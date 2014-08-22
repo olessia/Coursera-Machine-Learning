eaea9e5280d5ff2abb26bf11335adb507ec1dc44
Olessia Karpova <olessia.karpova@mail.utoronto.ca>
2014-08-22 15:49:32 -0400

    Dimensionality Reduction

---
+++ mlclass-ex7/computeCentroids.m
@@ -0,0 +1,45 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+for c=1:K
+    % find the indeces of all examles assigned to this cluster
+    indeces = find(idx == c);
+    % calculate the number of examples assigned to this cluster
+    num_examples = size(indeces, 1);
+    % extract the examples from all examples
+    examples = X(indeces, :);
+    % find the mean
+    centroids(c, :) = sum(examples, 1) / num_examples;
+end
+
+
+% =============================================================
+
+
+end
+
